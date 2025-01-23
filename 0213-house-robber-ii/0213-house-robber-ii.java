5a9c98969c304693ebd5f71f6437b374f8f51d15
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-01-23 23:16:59 +0530

    Time: 0 ms (100%), Space: 40.9 MB (78.3%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -2,14 +2,15 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         Integer[][] dp = new Integer[n][2];
-        return robHelper(n - 1, nums, false, dp);
+        // isFirstTaken - 0 (false), 1(true)
+        return robHelper(n - 1, nums, 0, dp);
     }
 
     // f(i) -> returns maximum sum for index 0 to i of nums.
-    private int robHelper(int i, int[] nums, boolean isFirstTaken, Integer[][] dp) {
+    private int robHelper(int i, int[] nums, int isFirstTaken, Integer[][] dp) {
 
         if (i == 0) {
-            if (isFirstTaken) {
+            if (isFirstTaken == 1) {
                 return 0;
             } else {
                 return nums[0];
@@ -20,12 +21,12 @@ class Solution {
         if (i < 0) {
             return 0;
         }
-        if (dp[i][isFirstTaken ? 1 : 0] != null) return dp[i][isFirstTaken ? 1 : 0];
+        if (dp[i][isFirstTaken] != null) return dp[i][isFirstTaken];
         
-        int pick = nums[i] + robHelper(i - 2, nums, i == nums.length - 1 ? true : isFirstTaken, dp);
+        int pick = nums[i] + robHelper(i - 2, nums, i == nums.length - 1 ? 1 : isFirstTaken, dp);
         int notPick = robHelper(i - 1, nums, isFirstTaken, dp);
 
-        return dp[i][isFirstTaken ? 1 : 0] = Math.max(pick, notPick);
+        return dp[i][isFirstTaken] = Math.max(pick, notPick);
 
     }
 }
