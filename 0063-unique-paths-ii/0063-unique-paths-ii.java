b5ce82eebd3663b9f8d67e7de4293ed675a0278e
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-01-26 16:22:23 +0530

    Time: 1 ms (19.94%), Space: 41.7 MB (64.33%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,24 +2,28 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
+        // d[i][j] -> stores total unique ways from (0,0) to (i,j)
         Integer[][] dp = new Integer[m][n];
-        return f(m - 1, n - 1, obstacleGrid, dp);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                        dp[i][j] = 0;
+                } else if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    int up = 0;
+                    int left = 0;
+                    if (i - 1 >= 0) {
+                        up = dp[i - 1][j];
+                    }
+                    if (j - 1 >= 0) {
+                        left = dp[i][j - 1];
+                    }
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 
-    // f(i, j) -> return total unique ways from (0,0) to (i,j)
-    private int f(int i, int j, int[][] grid, Integer[][] dp) {
-        if (i < 0 || j < 0) {
-            return 0;
-        }
-        if (grid[i][j] == 1) {
-            return 0;
-        }
-        if (i == 0 && j == 0) {
-            return 1;
-        }
-        if (dp[i][j] != null) return dp[i][j];
-        int up = f(i - 1, j, grid, dp);
-        int left = f(i, j - 1, grid, dp);
-        return dp[i][j] = up + left;
-    }
 }
