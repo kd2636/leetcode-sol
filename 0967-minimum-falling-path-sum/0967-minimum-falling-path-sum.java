fb9f6e835644666d4beacbd5c309421c85b90ec7
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-01-26 19:58:10 +0530

    Time: 11 ms (6.74%), Space: 45.3 MB (64.13%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,24 +1,31 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
-        int min = Integer.MAX_VALUE;
         // dp[i][j] -> stores minimum falling sum from 1st row till (i, j)
         Integer[][] dp = new Integer[n][n];
         for (int i = 0; i < n; i++) {
-            min = Math.min(min, f(n - 1, i, matrix, dp));
+            dp[0][i] = matrix[0][i];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int topLeft = Integer.MAX_VALUE;
+                int topRight = Integer.MAX_VALUE;
+                if (j - 1 >= 0) {
+                    topLeft = dp[i - 1][j - 1];
+                }
+                if (j + 1 < n) {
+                    topRight = dp[i - 1][j + 1];
+                }
+                dp[i][j] = matrix[i][j] + Math.min(topLeft, Math.min(dp[i - 1][j], topRight));
+            }
+        }
+        
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            min = Math.min(min, dp[n - 1][i]);
         }
         return min;
-    }
-
-    // f(i, j) -> returns minimum path sum from ist row to (i, j)
-    private int f(int i, int j, int[][] matrix, Integer[][] dp) {
-        if (j < 0 || j > matrix.length - 1) {
-            return Integer.MAX_VALUE;
-        }
-        if (i == 0) {
-            return matrix[i][j];
-        }
-        if (dp[i][j] != null) return dp[i][j];
-        return dp[i][j] = matrix[i][j] + Math.min(f(i - 1, j - 1, matrix, dp), Math.min(f(i - 1, j, matrix, dp), f(i - 1, j + 1, matrix, dp)));
     }
 }
