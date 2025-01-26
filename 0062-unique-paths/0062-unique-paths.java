e495418efc19a1edcaceda1a003744a47120638f
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-01-26 16:02:30 +0530

    Time: 1 ms (8.78%), Space: 40.4 MB (75.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,20 +1,25 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+        //dp[i][j] -> returns count of unique paths from (0,0) to (i,j)
         Integer[][] dp = new Integer[m][n];
-        return f(m - 1, n - 1, dp);
-        
-    }
-    // f(i, j) -> returns count of unique paths from (0,0) to (i,j)
-    private int f(int i, int j, Integer[][] dp) {
-        if (i == 0 && j == 0) {
-            return 1;
-        } else if (i < 0 || j < 0) {
-            return 0;
-        } else {
-            if (dp[i][j] != null) return dp[i][j];
-            int up = f(i - 1, j, dp);
-            int left = f(i, j - 1, dp);
-            return dp[i][j] = up + left;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else{
+                    int up = 0;
+                    int left = 0;
+                    if (i - 1 >= 0) {
+                        up = dp[i - 1][j];
+                    }
+                    if (j - 1 >= 0) {
+                        left = dp[i][j - 1];
+                    }
+                    dp[i][j] = up + left;
+                }
+            }
         }
+        return dp[m - 1][n - 1];
+        
     }
 }
