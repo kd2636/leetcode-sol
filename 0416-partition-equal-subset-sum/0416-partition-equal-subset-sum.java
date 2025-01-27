f799e536a5a743999e4a04dce11d270b98fb8af6
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-01-27 23:56:19 +0530

    Time: 43 ms (53.93%), Space: 47 MB (50.79%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -16,32 +16,27 @@ class Solution {
 
     private boolean isSubsetSumTarget(int[] nums, int k) {
         int n = nums.length;
-        Boolean[][] dp = new Boolean[n][k + 1];
-        return f(n - 1, k, nums, dp);
-    }
-
-    // f(i, k) -> return whether there is a subset with sum k from 0 to i;
-    private boolean f(int i, int target, int[] nums, Boolean[][] dp) {
-        if (target == 0) return true;
-        if (i == 0) {
-            if (nums[i] == target) return true;
-            else return false;
+        // dp[i][j] -> store if there is a subset with sum j from 0 to i
+        boolean[][] dp = new boolean[n][k + 1];
+        if (nums[0] <= k) {
+            dp[0][nums[0]] = true;
         }
 
-        if (dp[i][target] != null) return dp[i][target];
-        // not take
-        if (f(i - 1, target, nums, dp)) {
-            return dp[i][target] = true;
-        }
-
-        // take
-        if (target >= nums[i]) {
-            if (f(i - 1, target - nums[i], nums, dp)) {
-                return dp[i][target] = true;
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = true;
+            for (int target = 1; target <= k; target++) {
+                // not take
+                if (dp[i - 1][target]) {
+                    dp[i][target] = true;
+                } else if (nums[i] <= target && dp[i - 1][target - nums[i]]) {
+                    dp[i][target] = dp[i - 1][target - nums[i]];
+                } else {
+                    dp[i][target] = false;
+                }
             }
         }
 
-        return dp[i][target] = false;
-
+        return dp[n - 1][k];
+        
     }
 }
