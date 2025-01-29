1a95501f3780ea57f3196a99a30a859657bfd91c
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-01-29 21:55:00 +0530

    Time: 24 ms (36.08%), Space: 45.4 MB (20.99%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,41 +1,40 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        Integer[][] dp = new Integer[n][amount + 1];
-        return f(n - 1, amount, coins, dp);
+        // dp[i][amt] -> stores min number of coins required to get amt from 0 to i.
+        int[][] dp = new int[n][amount + 1];
+        dp[0][0] = 0;
+
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0) dp[0][i] = i / coins[0];
+            else dp[0][i] = -1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                int np = dp[i - 1][j];
+
+                int p = -1;
+                if (coins[i] <= j) {
+                    p = 1 + dp[i][j - coins[i]];
+                }
+
+                int ans = 0;
+                if (np == -1 && (p == 0 || p == -1)) {
+                    ans = -1;
+                } else if (np == -1) {
+                    ans = p;
+                } else if (p == 0 || p == -1) {
+                    ans = np;
+                } else {
+                    ans = Math.min(p, np);
+                }
+
+                dp[i][j] = ans;
+            }
+        }
+
+        return dp[n - 1][amount];
     }
 
-    // f(i, amt) -> returns min number of coins required to get amt from 0 to i.
-    private int f(int i, int amount, int[] coins, Integer[][] dp) {
-        if (i == 0) {
-          if (amount == 0) return 0;
-          if (amount % coins[0] == 0) return amount / coins[0];
-          return -1;
-        }
-
-        if (dp[i][amount] != null) return dp[i][amount];
-
-        // not pick coin[i]
-        int np = f(i - 1, amount, coins, dp);
-        
-
-        // pick coin[i]
-        int p = 0;
-        if (coins[i] <= amount) {
-            p = 1 + f(i, amount - coins[i], coins, dp);
-        }
-
-        int ans = 0;
-        if (np == -1 && p == 0) {
-            ans = -1;
-        } else if (np == -1) {
-            ans = p;
-        } else if (p == 0) {
-            ans = np;
-        } else {
-            ans = Math.min(p, np);
-        }
-
-        return dp[i][amount] = ans;
-    }
 }
