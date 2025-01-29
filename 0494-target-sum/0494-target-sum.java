56f586a3dad80c4239f4e1a1465147dd64a4b858
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-01-30 00:11:53 +0530

    Time: 4 ms (93.63%), Space: 44.3 MB (35.04%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,24 +1,41 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
+        // problem can be boiled down to finding two subsets with diff d.
+        // s1 - s2 = d
+        // s1 - (totalSum - s1) = d
+        // s1 = (d + totalSum) / 2
         int n = nums.length;
-
-        Map<String, Integer> dp = new HashMap<>();
-        return f(n - 1, target, nums, dp);
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = sum + nums[i];
+        }
+        int d = target;
+        if (target < 0) d = -1 * target;
+        
+        if ((d + sum) % 2 != 0) return 0;
+        int targetSum = (d + sum) / 2;
+        Integer[][] dp = new Integer[n][targetSum + 1];
+        return f(n - 1, targetSum, nums, dp);
     }
 
-    // f(i, target) -> return number if expressions from 0 to i with sum = target
-    private int f(int i, int target, int[] nums, Map<String, Integer> dp) {
+    // f(i, target) -> return count of subsets from 0 to i with sum = target
+    private int f(int i, int target, int[] nums, Integer[][] dp) {
         if (i == 0) {
-            if ((target - nums[0] == 0) || (target + nums[0] == 0)) {
-                if (nums[0] == 0) return 2;
-                return 1;
-            }
+            if (target == 0 && nums[0] == target) return 2;
+            if (target == 0) return 1;
+            if (target == nums[i]) return 1;
             return 0;
         }
-        String key = i + "*" + target;
-        if (dp.get(key) != null) return dp.get(key);
-        int ans = f(i - 1, target - nums[i], nums, dp) + f(i - 1, target + nums[i], nums, dp);
-        dp.put(key, ans);
-        return ans;
+
+        if (dp[i][target] != null) return dp[i][target];
+
+        int nt = f(i - 1, target, nums, dp);
+
+        int t = 0;
+        if (nums[i] <= target) {
+            t = f(i - 1, target - nums[i], nums, dp);
+        }
+
+        return dp[i][target] = nt + t;
     }
 }
