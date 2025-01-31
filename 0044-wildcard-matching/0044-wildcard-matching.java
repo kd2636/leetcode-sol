7648d54a8df0315906f2e40be4f7591b3ec2949f
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-02-01 02:05:25 +0530

    Time: 21 ms (44.64%), Space: 44.6 MB (80.55%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -2,27 +2,32 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n = s.length();
         int m = p.length();
-        boolean[][] dp = new boolean[n + 1][m + 1];
+        boolean[] prev = new boolean[m + 1];
+        boolean[] curr = new boolean[m + 1];
 
-        dp[0][0] = true;
+        prev[0] = true;
         for (int j = 1; j <= m; j++) {
-            if (dp[0][j - 1] && p.charAt(j - 1) == '*') {
-                dp[0][j] = true;
+            if (prev[j - 1] && p.charAt(j - 1) == '*') {
+                prev[j] = true;
             }
         }
 
         for (int i = 1; i <= n; i++) {
+            curr[0] = false;
             for (int j = 1; j <= m; j++) {
                 if ((s.charAt(i - 1) == p.charAt(j - 1)) || p.charAt(j - 1) == '?') {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = prev[j - 1];
                 } else if (p.charAt(j - 1) == '*') {
-                    dp[i][j] = dp[i][j - 1] || (dp[i - 1][j] || dp[i - 1][j - 1]);
+                    curr[j] = curr[j - 1] || (prev[j] || prev[j - 1]);
                 } else {
-                    dp[i][j] = false;
+                    curr[j] = false;
                 }                
             }
+            boolean[] temp = prev;
+            prev = curr;
+            curr = temp;
         }
-        return dp[n][m];
+        return prev[m];
     }
 
 }
