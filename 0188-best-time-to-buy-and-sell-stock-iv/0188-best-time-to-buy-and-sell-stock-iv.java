6474b43e50202ef5a17201ab0f87cd8f8f5bb6e1
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-02-02 02:12:00 +0530

    Time: 2 ms (78.52%), Space: 44 MB (48.06%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,26 +1,22 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        Integer[][] dp = new Integer[n][2 * k + 1];
-        return f(0, 2 * k, prices, dp);
+        int[][] dp = new int[n + 1][2 * k + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int cap = 1; cap <= (2 * k); cap++) {
+                if (cap % 2 == 0) {
+                    int a = -prices[i] + dp[i + 1][cap - 1];
+                    int b = dp[i + 1][cap];
+                    dp[i][cap] = Math.max(a, b);
+                } else {
+                    int a = prices[i] + dp[i + 1][cap - 1];
+                    int b = dp[i + 1][cap];
+                    dp[i][cap] = Math.max(a, b);
+                }
+            }
+        }
+        return dp[0][2 * k];
     }
 
-    // f(i, cap) -> return max profit from i to n with cap transactions.
-    // cap -> even : buy
-    // cap -> odd : sell
-    private int f(int i, int cap, int[] prices, Integer[][] dp) {
-        if (cap == 0 || i == prices.length) {
-            return 0;
-        }
-        if (dp[i][cap] != null) return dp[i][cap];
-        if (cap % 2 == 0) {
-            int a = -prices[i] + f(i + 1, cap - 1, prices, dp);
-            int b = f(i + 1, cap, prices, dp);
-            return dp[i][cap] = Math.max(a, b);
-        } else {
-            int a = prices[i] + f(i + 1, cap - 1, prices, dp);
-            int b = f(i + 1, cap, prices, dp);
-            return dp[i][cap] = Math.max(a, b);
-        }
-    }
 }
