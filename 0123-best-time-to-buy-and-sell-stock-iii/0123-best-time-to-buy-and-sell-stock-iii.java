89eab54b562d2eaf3d7bd45f0166b71274da5624
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-02-02 01:27:15 +0530

    Time: 7 ms (50.84%), Space: 57.4 MB (98.81%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,25 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n + 1][2][3];
+        int[][] next = new int[2][3];
+        int[][] curr = new int[2][3];
 
         for (int i = n - 1; i >= 0; i--) {
             for (int j = 0; j <= 1; j++) {
                 for (int k = 1; k < 3; k++) {
                     if (j == 0) {
-                        int a = -prices[i] + dp[i + 1][1][k];
-                        int b = dp[i + 1][0][k];
-                        dp[i][j][k] = Math.max(a, b);
+                        int a = -prices[i] + next[1][k];
+                        int b = next[0][k];
+                        curr[j][k] = Math.max(a, b);
                     } else {
-                        int a = prices[i] + dp[i + 1][0][k - 1];
-                        int b = dp[i + 1][1][k];
-                        dp[i][j][k] = Math.max(a, b);
+                        int a = prices[i] + next[0][k - 1];
+                        int b = next[1][k];
+                        curr[j][k] = Math.max(a, b);
                     }
                 }
             }
+            int[][] temp = next;
+            next = curr;
+            curr = temp;
         }
 
-        return dp[0][0][2];
+        return next[0][2];
         
     }
 }
