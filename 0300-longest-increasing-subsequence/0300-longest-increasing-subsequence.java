865de3eb181a1248ceda1b0e1556481228a341e8
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-02-02 20:34:15 +0530

    Time: 5 ms (91.73%), Space: 44.3 MB (55.4%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -7,19 +7,22 @@ class Solution {
         List<Integer> temp = new ArrayList<>();
         temp.add(nums[0]);
 
+        int ans = 1;
+
         for (int i = 1; i < n; i++) {
             int binaryIndex = Collections.binarySearch(temp, nums[i]);
             if (binaryIndex < 0) {
                 binaryIndex = -binaryIndex - 1;
                 if (binaryIndex == temp.size()) {
                     temp.add(nums[i]);
+                    ans++;
                 } else {
                     temp.set(binaryIndex, nums[i]);
                 }                
             } 
         }
 
-        return temp.size();
+        return ans;
 
     }
 
