aba21c30a6d775ed121324309564971884d290e5
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-02-02 13:08:56 +0530

    Time: 66 ms (26.69%), Space: 77.6 MB (14.64%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -4,15 +4,15 @@ class Solution {
         int[][] dp = new int[n + 1][n + 1];
 
         for (int i = n - 1; i >= 0; i--) {
-            for (int prev_i = i - 1; prev_i >= -1; prev_i--) {
-                int nt = dp[i + 1][prev_i + 1];
+            for (int prev_i = 0; prev_i <= i; prev_i++) {
+                int nt = dp[i + 1][prev_i];
 
                 int t = 0;
-                if (prev_i == -1 || nums[i] > nums[prev_i]) {
+                if (prev_i == 0 || nums[i] > nums[prev_i - 1]) {
                     t = 1 + dp[i + 1][i + 1];
                 }
 
-                dp[i][prev_i + 1] = Math.max(nt, t);
+                dp[i][prev_i] = Math.max(nt, t);
             }
         }
         return dp[0][0];
