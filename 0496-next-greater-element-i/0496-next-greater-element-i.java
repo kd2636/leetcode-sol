0ef180bf3ff56d2ef6089fac5361881d6f0be742
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-02-08 11:58:32 +0530

    Time: 4 ms (41.7%), Space: 44.8 MB (8.34%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,9 +1,6 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] ans = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            ans[i] = -1;
-        }
 
         Map<Integer, Integer> map = new HashMap<>();
 
@@ -12,16 +9,20 @@ class Solution {
         }
 
 
-        for (int i = 0; i < nums2.length; i++) {
-            boolean foundNge = false;
-            for (int j = i + 1; j < nums2.length; j++) {
-                if (nums2[j] > nums2[i]) {
-                    if (map.containsKey(nums2[i])) {
-                        ans[map.get(nums2[i])] = nums2[j];
-                    }
-                    break;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                stack.pop();
+            }
+            if (map.containsKey(nums2[i])) {
+                if (stack.isEmpty()) {
+                    ans[map.get(nums2[i])] = -1;
+                } else {
+                    ans[map.get(nums2[i])] = stack.peek();
                 }
             }
+            stack.push(nums2[i]);
         }
 
         return ans;
