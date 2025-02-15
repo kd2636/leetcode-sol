3f1a55f22b78f7147bb3355ba2a44ca8e43d56c8
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-02-15 20:05:18 +0530

    Time: 3 ms (88.33%), Space: 47.1 MB (6.42%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        // all you need is to find the longest subarrays with max 0s upto k
+
+        int l = 0;
+        int r = 0;
+        int zeroes = 0;
+        int n = nums.length;
+        int maxLength = 0;
+
+        while (r < n) {
+            if (nums[r] == 0) {
+                zeroes++;
+            }
+
+            while (zeroes > k) {
+                if (nums[l] == 0) {
+                    zeroes--;
+                }
+                l++;
+            }
+
+            if (zeroes <= k) {
+                maxLength = Math.max(maxLength, r - l + 1);
+            }
+
+            r++;
+        }
+
+        return maxLength;
+    }
+}
