6d0edfb0ca694db80fa1d135db12867cdbbf8a58
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-02-16 12:24:24 +0530

    Time: 50 ms (63.13%), Space: 55 MB (77.28%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -7,21 +7,16 @@ class Solution {
         int maxLength = 0;
 
         while (r < fruits.length) {
-            if (map.getOrDefault(fruits[r], 0) == 0) {
-                dc++;
-            }
-
-            while (dc > 2) {
-                if (map.getOrDefault(fruits[l], 0) == 1) {
-                    map.put(fruits[l], 0);
-                    dc--;
+            map.put(fruits[r], map.getOrDefault(fruits[r], 0) + 1);
+            while (map.size() > 2) {
+                if (map.getOrDefault(fruits[l], 0) == 1) {    
+                    map.remove(fruits[l]);
                 } else {
                     map.put(fruits[l], map.getOrDefault(fruits[l], 0) - 1);
                 }
                 l++;
             }
 
-            map.put(fruits[r], map.getOrDefault(fruits[r], 0) + 1);
             maxLength = Math.max(maxLength, r - l + 1);
             r++;
         }
