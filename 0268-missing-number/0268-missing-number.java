4cbaa6a7ccff8d31a03a4c7470d24b5bc11b8e34
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-02-19 23:45:40 +0530

    Time: 0 ms (100%), Space: 45.3 MB (69.85%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -3,14 +3,13 @@ class Solution {
         int n = nums.length;
 
         int xor1 = 0;
-        for (int i = 0; i <= n; i++) {
-            xor1 = xor1 ^ i;
-        }
-        
         int xor2 = 0;
+        
         for (int i = 0; i < n; i++) {
+            xor1 = xor1 ^ i;
             xor2 = xor2 ^ nums[i];
         }
+        xor1 = xor1 ^ n;
 
         return xor1 ^ xor2;
     }
