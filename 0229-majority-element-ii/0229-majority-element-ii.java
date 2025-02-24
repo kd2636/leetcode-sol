7cffdc5026d861180f4b9f44e9d4744ead0ed6cf
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-02-25 00:08:03 +0530

    Time: 2 ms (99.12%), Space: 48.4 MB (29%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,46 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        // based on Moore voiting algo
         List<Integer> ans = new ArrayList<>();
         int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
+        int count1 = 0, count2 = 0;
+        int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE;
+        
 
-        for (int key : map.keySet()) {
-            if (map.get(key) > n / 3) {
-                ans.add(key);
+        for (int num : nums) {
+            if (count1 == 0 && num != ele2) {
+                count1 = 1;
+                ele1 = num;
+            } else if (count2 == 0 && num != ele1) {
+                count2 = 1;
+                ele2 = num;
+            } else if (num == ele1) {
+                count1++;
+            } else if (num == ele2) {
+                count2++;
+            } else {
+                count1--;
+                count2--;
             }
         }
+
+        if (isMajority(nums, ele1)) {
+            ans.add(ele1);
+        }
+        if (isMajority(nums, ele2)) {
+            ans.add(ele2);
+        }
         return ans;
+
+    }
+
+    private boolean isMajority(int[] nums, int ele) {
+        int n = nums.length;
+        int count = 0;
+        for (int num : nums) {
+            if (num == ele) count++;
+        }
+
+        return count > n / 3;
     }
 }
