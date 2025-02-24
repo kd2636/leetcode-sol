304dfc07af059b32994ada6a93d628ecfaafa3f2
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-02-25 01:28:21 +0530

    Time: 31 ms (58.58%), Space: 51.5 MB (59.59%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,26 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // brute 
         List<List<Integer>> ans = new ArrayList<>();
-        Set<List<Integer>> set = new HashSet<>();
         int n = nums.length;
-
+        Arrays.sort(nums);
         for (int i = 0; i < n; i++) {
-            Set<Integer> visited = new HashSet<>();
-            for (int j = i + 1; j < n; j++) {
-                if (visited.contains(-(nums[j] + nums[i]))) {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(-(nums[j] + nums[i]));
-                    Collections.sort(list);
-                    set.add(list);
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1;
+            int k = n - 1;
+            
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    List<Integer> list = List.of(nums[i], nums[j], nums[k]);
+                    ans.add(list);
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
                 }
-                visited.add(nums[j]);
             }
         }
 
-        ans.addAll(set);
         return ans;
     }
 }
