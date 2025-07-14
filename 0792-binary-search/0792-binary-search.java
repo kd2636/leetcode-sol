3ecdf84f91570225734c1e1ed1653190f95517c7
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-07-15 00:43:37 +0530

    Time: 0 ms (100%), Space: 45.8 MB (60.41%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,20 +1,17 @@
 class Solution {
     public int search(int[] nums, int target) {
-
-        int l = 0;
-        int r = nums.length - 1;
-
-        while (l <= r) {
-            int mid = (l + r) / 2;
+        int beg = 0;
+        int last = nums.length - 1;
+        while (beg <= last) {
+            int mid = (beg + last) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (target < nums[mid]) {
-                r = mid - 1;
+                last = mid - 1;
             } else {
-                l = mid + 1;
+                beg = mid + 1;
             }
         }
         return -1;
-        
     }
 }
