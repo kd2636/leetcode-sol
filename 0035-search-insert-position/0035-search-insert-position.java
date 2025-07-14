ab91848165330b7b14a9e34c720163dc73740aad
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-07-15 00:48:34 +0530

    Time: 0 ms (100%), Space: 43.3 MB (18.43%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,16 +2,18 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int l = 0;
         int r = nums.length - 1;
-
+        int lb = -1;
         while (l <= r) {
             int mid = (l + r) / 2;
-            if (nums[mid] >= target) {
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                lb = mid;
                 r = mid - 1;
             } else {
                 l = mid + 1;
             }
         }
-
-        return l;
+        return lb == -1 ? nums.length : lb;
     }
 }
