47ddf47cd0d47a63d62a0f1879d465e6bc5fd821
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-09-09 16:17:28 +0530

    Time: 0 ms (100%), Space: 40.3 MB (66.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-        return f(n);
+        Integer[] dp = new Integer[n + 1];
+        return f(n, dp);
     }
 
     // f(i) -> 0 to i
 
-    private int f(int i) {
+    private int f(int i, Integer[] dp) {
         if (i < 0) return 0;
         if (i == 0) return 1;
 
-        return f(i - 1) + f(i - 2);
+        if (dp[i] != null) return dp[i];
+
+        return dp[i] = f(i - 1, dp) + f(i - 2, dp);
     }
 }
