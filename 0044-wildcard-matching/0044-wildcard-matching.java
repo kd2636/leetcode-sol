667f361900be960b22a788a92a6305f2191d2d88
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-09-13 13:29:15 +0530

    Time: 25 ms (14.78%), Space: 44.8 MB (63.28%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -2,28 +2,32 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int l1 = s.length();
         int l2 = p.length();
-        Boolean[][] dp = new Boolean[l1][l2];
-        return f(s, l1 - 1, p, l2 - 1, dp);
-    }
+        boolean[][] dp = new boolean[l1 + 1][l2 + 1];
 
-    private boolean f(String s, int i, String p, int j, Boolean[][] dp) {
-        if (i < 0 && j < 0) return true;
-        if (j < 0) return false;
-        if (i < 0) {
-            for (int k = 0; k <= j; k++) {
-                if (p.charAt(k) != '*') return false;
+        dp[0][0] = true;
+        for (int i = 1; i <= l1; i++) {
+            dp[i][0] = false;
+        }
+
+        for (int i = 1; i <= l2; i++) {
+            if (p.charAt(i - 1) == '*')
+                dp[0][i] = true;
+            else {
+                break;
+            } 
+        }
+
+        for (int i = 1; i <= l1; i++) {
+            for (int j = 1; j <= l2; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - 1] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = false;
+                }
             }
-            return true;
         }
-
-        if (dp[i][j] != null) return dp[i][j];
-
-        if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?') {
-            return dp[i][j] = f(s, i - 1, p, j - 1, dp);
-        } else if (p.charAt(j) == '*') {
-            return dp[i][j] = f(s, i - 1, p, j, dp) || f(s, i - 1, p, j - 1, dp) || f (s, i, p, j - 1, dp);
-        } else {
-            return dp[i][j] = false;
-        }
+        return dp[l1][l2];
     }
 }
