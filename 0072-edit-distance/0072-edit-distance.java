be1dc06717cfc5b7d1b91e2fddc3ec0933136572
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-09-13 10:28:49 +0530

    Time: 5 ms (60.87%), Space: 45.3 MB (30.66%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,35 +1,28 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-
-        int[] prev = new int[m + 1];
-        int[] curr = new int[m + 1];
-
-
-        for (int j = 1; j <= m; j++) {
-            prev[j] = j;
-        }
-
-        for (int i = 1; i <= n; i++) {
-            curr[0] = i;
-            for (int j = 1; j <= m; j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    curr[j] = prev[j - 1];
-                } else {
-                    int insertion = curr[j - 1];
-                    int replacement = prev[j - 1];
-                    int deletion = prev[j];
-
-                    curr[j] = 1 + Math.min(insertion, Math.min(replacement, deletion));
-                }
-            }
-            int[] temp = prev;
-            prev = curr;
-            curr = temp;
-        }
-
-        return prev[m];
+        // f(i, j) -> return min operation to convert s1 (0 to i) to s2 (0 to j)
+        int l1 = word1.length();
+        int l2 = word2.length();
+        Integer[][] dp = new Integer[l1][l2];
+        return f(word1, l1 - 1, word2, l2 - 1, dp);
     }
 
+    private int f(String s1, int i, String s2, int j, Integer[][] dp) {
+        if (i < 0) return j + 1;
+        if (j < 0) return i + 1;
+
+        if (dp[i][j] != null) return dp[i][j];
+
+        int minOperations = 0;
+        if (s1.charAt(i) == s2.charAt(j)) {
+            minOperations = Math.min(f(s1, i - 1, s2, j - 1, dp), 1 + f(s1, i - 1, s2, j, dp));
+        } else {
+            int insert = 1 + f(s1, i, s2, j - 1, dp);
+            int delete = 1 + f(s1, i - 1, s2, j, dp);
+            int replace = 1 + f(s1, i - 1, s2, j - 1, dp);
+            minOperations = Math.min(insert, Math.min(delete, replace));
+        }
+
+        return dp[i][j] = minOperations;
+    }
 }
