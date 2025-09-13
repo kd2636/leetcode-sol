e5629481e90e2808667da26bf90b3a4260cfbbdf
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-09-13 10:37:55 +0530

    Time: 4 ms (72.67%), Space: 44.8 MB (93.11%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -3,32 +3,32 @@ class Solution {
         // f(i, j) -> return min operation to convert s1 (0 to i) to s2 (0 to j)
         int l1 = word1.length();
         int l2 = word2.length();
-        int[][] dp = new int[l1 + 1][l2 + 1];
+        int[] prev = new int[l2 + 1];
 
         for (int i = 0; i <= l2; i++) {
-            dp[0][i] = i;
-        }
-        for (int i = 0; i <= l1; i++) {
-            dp[i][0] = i;
+            prev[i] = i;
         }
 
         for (int i = 1; i <= l1; i++) {
+            int[] curr = new int[l2 + 1];
+            curr[0] = i;
             for (int j = 1; j <= l2; j++) {
                 int minOperations = 0;
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    minOperations = Math.min(dp[i - 1][j - 1], 1 + dp[i - 1][j]);
+                    minOperations = Math.min(prev[j - 1], 1 + prev[j]);
                 } else {
-                    int insert = 1 + dp[i][j - 1];
-                    int delete = 1 + dp[i - 1][j];
-                    int replace = 1 + dp[i - 1][j - 1];
+                    int insert = 1 + curr[j - 1];
+                    int delete = 1 + prev[j];
+                    int replace = 1 + prev[j - 1];
                     minOperations = Math.min(insert, Math.min(delete, replace));
                 }
-                dp[i][j] = minOperations;
+                curr[j] = minOperations;
             }
+            prev = curr;
         }
 
 
-        return dp[l1][l2];
+        return prev[l2];
     }
 
 }
