2ac549ee4826bfe4f9f5075d53879a02645eabd0
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-09-16 00:42:36 +0530

    Time: 1 ms (82.85%), Space: 41.9 MB (45.26%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,27 +1,21 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int[] next = new int[3];
-        int[] curr = new int[3];
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = 0; j < 3; j++) {
-                if (j == 0) {
-                    int a = -prices[i] + next[1];
-                    int b = next[0];
-                    curr[j] = Math.max(a, b);
-                } else if (j == 1) {
-                    int a = prices[i] + next[2];
-                    int b = next[1];
-                    curr[j] = Math.max(a, b);
-                } else {
-                    curr[j] = next[0];
-                }
-            }
-            int[] temp = next;
-            next = curr;
-            curr = temp;
-        }
-        return next[0];
+        int n = prices.length; 
+        Integer[][] dp = new Integer[n][2];
+        return f(0, 0, prices, dp);
     }
 
+    private int f(int i, int j, int[] prices, Integer[][] dp) {
+        if (i >= prices.length) {
+            return 0;
+        }
+
+        if (dp[i][j] != null) return dp[i][j];
+
+        if (j == 0) {
+            return dp[i][j] = Math.max(f(i + 1, 0, prices, dp), -prices[i] + f(i + 1, 1, prices, dp));
+        } else {
+            return dp[i][j] = Math.max(f(i + 1, 1, prices, dp), prices[i] + f(i + 2, 0, prices, dp));
+        }
+    }
 }
