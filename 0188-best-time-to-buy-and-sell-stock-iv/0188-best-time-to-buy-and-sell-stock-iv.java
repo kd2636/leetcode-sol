65a996710655b4fd3a289746d4ec3721cd6bf809
Kushagra Dalmia <dalmia.kushagra@gmail.com>
2025-09-15 23:40:44 +0530

    Time: 2 ms (82.18%), Space: 44.4 MB (53.37%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -2,24 +2,23 @@ class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
 
-        Integer[][][] dp = new Integer[n][2][k + 1];
-        return f(0, 0, k, prices, dp);
-        
-    }
+        int[][][] dp = new int[n + 1][2][k + 1];
 
-    private int f(int i, int j, int k, int[] prices, Integer[][][] dp) {
-        if (i == prices.length) return 0;
-        if (k == 0) return 0;
-
-        if (dp[i][j][k] != null) return dp[i][j][k];
-
-        if (j == 0) {
-            // can buy
-            return dp[i][j][k] = Math.max(f(i + 1, 0, k, prices, dp), -prices[i] + f(i + 1, 1, k, prices, dp));
-        } else {
-            // can sell only
-            return dp[i][j][k] = Math.max(f(i + 1, 1, k, prices, dp), prices[i] + f(i + 1, 0, k - 1, prices, dp));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < 2; j++) {
+                for (int x = 1; x <= k; x++) {
+                    if (j == 0) {
+                        // can buy
+                        dp[i][j][x] = Math.max(dp[i + 1][0][x], -prices[i] + dp[i + 1][1][x]);
+                    } else {
+                        // can sell only
+                        dp[i][j][x] = Math.max(dp[i + 1][1][x], prices[i] + dp[i + 1][0][x - 1]);
+                    }
+                }
+            }
         }
 
+        return dp[0][0][k];
+        
     }
 }
